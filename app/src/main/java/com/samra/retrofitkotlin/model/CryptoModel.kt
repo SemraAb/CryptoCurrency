a0f9eb240aaeb82a16596980125d3c6dac52758d
Samra Abdurahmanli <samra.abdurahmanli.a@asoiu.edu.az>
2023-09-19 11:51:32 +0400

    model, and adapter was added

--- app/src/main/java/com/samra/retrofitkotlin/model/CryptoModel.kt
+++
@@ -1,9 +0,0 @@
-package com.samra.retrofitkotlin.model
-
-import com.google.gson.annotations.SerializedName
-
-data class CryptoModel(
-    @SerializedName("name")
-    var currency :String ,
-    var price: String) {
-}

6b5e9a872b13a50f505125cb70a1286b4cae28c4
Samra Abdurahmanli <samra.abdurahmanli.a@asoiu.edu.az>
2023-09-19 13:07:17 +0400

    Adapter and model was added

--- app/src/main/java/com/samra/retrofitkotlin/service/CryptoApi.kt
+++ app/src/main/java/com/samra/retrofitkotlin/service/CryptoApi.kt
@@ -7,7 +7,7 @@ import retrofit2.http.GET
 interface CryptoApi {
     //https://pro-api.coinmarketcap.com/v1/
     // cryptocurrency/listings/latest?CMC_PRO_API_KEY=8ee21f53-aa6b-4d50-853e-5725d7c44334
-@GET("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/")
-fun getData(): Call<List<CryptoModel>>
+@GET("cryptocurrency/listings/latest?CMC_PRO_API_KEY=8ee21f53-aa6b-4d50-853e-5725d7c44334")
+fun getData(): Call<CryptoModel>
 
 }
--- app/src/main/java/com/samra/retrofitkotlin/view/MainActivity.kt
+++ app/src/main/java/com/samra/retrofitkotlin/view/MainActivity.kt
@@ -2,8 +2,16 @@ package com.samra.retrofitkotlin.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.LayoutManager
 import com.samra.retrofitkotlin.R
+import com.samra.retrofitkotlin.adapter.CryptoAdapter
+import com.samra.retrofitkotlin.databinding.ActivityMainBinding
 import com.samra.retrofitkotlin.model.CryptoModel
+import com.samra.retrofitkotlin.model.DataModel
 import com.samra.retrofitkotlin.service.CryptoApi
 import retrofit2.Call
 import retrofit2.Callback
@@ -11,13 +19,19 @@ import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class MainActivity : AppCompatActivity() {
-    private val BASE_URL = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/"
-    private var cryptoModels:ArrayList<CryptoModel>? = null
+class MainActivity : AppCompatActivity() , CryptoAdapter.Listener {
+private lateinit var binding:ActivityMainBinding
+    private val BASE_URL = "https://pro-api.coinmarketcap.com/v1/"
+    private var cryptoModels:CryptoModel? = null
+    private lateinit var recyclerAdapter:CryptoAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         loadData()
+        var layoutManager : RecyclerView.LayoutManager=LinearLayoutManager(this)
+        binding.recyclerView.layoutManager = layoutManager
+
     }
 //    https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/
 // latest?CMC_PRO_API_KEY=8ee21f53-aa6b-4d50-853e-5725d7c44334
@@ -30,27 +44,26 @@ class MainActivity : AppCompatActivity() {
 
         var service = retrofit.create(CryptoApi::class.java)
         var call = service.getData()
-        call.enqueue(object: Callback<List<CryptoModel>>{
+        call.enqueue(object: Callback<CryptoModel>{
             override fun onResponse(
-                call: Call<List<CryptoModel>>,
-                response: Response<List<CryptoModel>>
+                call: Call<CryptoModel>,
+                response: Response<CryptoModel>
             ) {
                 if(response.isSuccessful){
-                    response.body()?.let{
-                        cryptoModels = ArrayList(it)
+                        recyclerAdapter = CryptoAdapter(response.body()!! ,this@MainActivity )
+                        binding.recyclerView.adapter = recyclerAdapter
 
-                        for(cryptoModel: CryptoModel in cryptoModels!!){
-
-                            println(cryptoModel)
-                        }
-                    }
                 }
             }
 
-            override fun onFailure(call: Call<List<CryptoModel>>, t: Throwable) {
+            override fun onFailure(call: Call<CryptoModel>, t: Throwable) {
                t.printStackTrace()
             }
 
         })
+    }
+
+    override fun onItemCLicked(cryptoModel: DataModel) {
+        Toast.makeText(this , "CLicked: ${cryptoModel.name}" , Toast.LENGTH_LONG).show()
     }
 }
